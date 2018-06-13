546bbde59dacb5c2ab6f2f3c64d736e91b4dce31
Ivan Kolmychek <ivan.kolmychek@gmail.com>
2018-06-13 20:51:48 +0200

    Try to reproduce the bug in processor

--- app/workers/processor.rb
+++ app/workers/processor.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 class Processor
   include Sneakers::Worker
   from_queue :logs
 
-
-  def work(msg)
-    Airbrake.notify(msg)
-    1/0
+  def work_with_params(raw_message, _delivery_info, _metadata)
+    raise 'whatever'
     ack!
+  rescue RuntimeError => e
+    # re-raise for whatever reasons
+    raise(e)
   end
 end
